66f1f9fe0a0290c77fb7970de7175ddca775211c
KongkongNT15 <kongkong745837@gmail.com>
2026-04-05 01:22:41 +0900

    ふははは

---
+++ src/WebPageGenerator/PageEditor/Pages/PageItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageEditor.Pages
+{
+    public class PageItem
+    {
+        public string Name { get; set; }
+
+        public string FolderPath { get; }
+
+        public PageType Type { get; }
+
+        public ObservableCollection<PageItem> Children { get; }
+
+        public PageItem(string folderPath, PageType type)
+        {
+            Name = string.Empty;
+            FolderPath = folderPath;
+            Type = type;
+            Children = [];
+        }
+    }
+}
---
+++ src/WebPageGenerator/PageEditor/Pages/PageType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageEditor.Pages
+{
+    public enum PageType
+    {
+        Class,
+        Enum,
+        Field,
+        Function,
+        Method,
+        Namespace,
+        Operator,
+    }
+}
